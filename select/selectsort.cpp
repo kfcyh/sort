fbdde243f33d914c476ed2f72bab6d1dde15c2f8
kfcyh <40755350+kfcyh@users.noreply.github.com>
2020-04-28 11:40:29 +0800

    Update selectsort.cpp

--- select/selectsort.cpp
+++ select/selectsort.cpp
@@ -1,5 +1,13 @@
-#include "sort.h"
+/*************************************************
+*												 *
+* Function:SelectSort							 *
+* Written by Mr.YeHeng							 *
+* 2020/4/28  Huaqiao University					 *
+*												 *
+*************************************************/
 
+#include "sort.h"
+/**********************简单选择排序*********************/
 void SelectSort(SqList L)
 {
 	int j, min;
@@ -14,4 +22,22 @@ void SelectSort(SqList L)
 		if (i != min)
 			swap(L, i, min);
 	}
+}
+/*************************测试代码************************/
+int main()
+{
+	SqList L = (SqList)new list;
+	int arc[] = { 9,1,5,8,3,7,4,6,2 };
+	for (int i = 0; i < 9; i++)
+	{
+		L->r[i + 1] = arc[i];
+		L->length++;
+	}
+	//L->length = 9;
+	SelectSort(L);
+	for (int i = 0; i < 9; i++)
+	{
+		cout << L->r[i + 1] << " ";
+	}
+	getchar();
 }
