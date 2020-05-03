366c45e1000db0cc1d75cec46c07a028d2992777
kfcyh <40755350+kfcyh@users.noreply.github.com>
2020-05-03 11:44:39 +0800

    Update and rename insertsort to insertsort.cpp

---
+++ insertsort/insertsort.cpp
@@ -0,0 +1,23 @@
+/*************************************************
+*						 *
+* Function:SelectSort			         *
+* Written by Mr.YeHeng				 *
+* 2020/4/28  Huaqiao University			 *
+*						 *
+*************************************************/
+#include "sort.h"
+
+void insertsort(SqList L)
+{
+	int i, j;
+	for (i = 2; i < L->length; i++)
+	{
+		if (L->r[i] < L->r[i - 1])
+		{
+			L->r[0] = L->r[i];
+			for (j = i - 1; L->r[j] > L->r[0]; j--)
+				L->r[j + 1] = L->r[j];
+			L->r[j + 1] = L->r[0];
+		}
+	}
+}
